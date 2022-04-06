b1fe286efc9660103a91394fe272a31cdaecd057
Izumi <izuminft@gmail.com>
2022-04-05 22:21:09 -0400

    color change / text edit

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the OmniApes!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = "The OmniApes is an omnichain NFT collection built on @LayerZero_Labs. OmniApes can be transferred to any supported chains (Ethereum, BNB Chain, Avalanche, Polygon, Arbitrum, Fantom, Optimism)";
+const welcome_p = "An omnichain NFT collection built on @LayerZero_Labs. OmniApes can be transferred to any supported chains (Ethereum, BNB Chain, Avalanche, Polygon, Arbitrum, Fantom, Optimism)";
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
